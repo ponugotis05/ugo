f47c9ede3ae54c02a67d26e4780c9f93490ec235
ponugotis05 <ponugotis05@users.noreply.github.com>
2017-04-18 23:44:17 -0400

    Add files via upload

---
+++ primitivedatatype.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package ugo.sp1.apr18;
+
+/**
+ * @author sharath
+ *
+ */
+public class primitivedatatype {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 100, i = -50;
+		char  name = 't';
+		boolean b	= true;
+		byte  y = -126;
+		float	f	=	2530.32f;
+		double	d	=	2533.32d;
+		short	s	=	1024;
+		long	l	= 253215;
+		
+		
+		System.out.println("print the primitive data type: int :" +a  + "\t" +i);
+		System.out.println("print the primitive data type: char:" +name);
+		System.out.println("print the primitive data type: boolean:" +b);
+		System.out.println("print the primitive data type: byte:" +y);
+		System.out.println("print the primitive data type: float:" +f);
+		System.out.println("print the primitive data type: double:" +d);
+		System.out.println("print the primitive data type: short:" +s);
+		System.out.println("print the primitive data type: long:" +l);
+	}
+
+}
