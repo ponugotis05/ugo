0a49542b17ee10098d62dd5cf7c6d12d1916a78a
ponugotis05 <ponugotis05@users.noreply.github.com>
2017-04-20 13:35:49 -0400

    Add files via upload

---
+++ CallingObject.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package ugo.sp1.apr18;
+
+
+/**
+ * @author sharath
+ *
+ */
+public class CallingObject {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		PrimitiveDataType primitivedatatype1 = new PrimitiveDataType();
+		primitivedatatype1.datatype();
+
+	}
+
+}
